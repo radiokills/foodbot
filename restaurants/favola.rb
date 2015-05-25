8267a902aa1889a83ea88aa6e33227313f22d63d
Kristjan Hoareau <kristjan.hoareau@dlabs.si>
2015-05-25 14:01:07 +0200

    Added favola restaurant

---
+++ restaurants/favola.rb
@@ -0,0 +1,16 @@
+class Favola < Restaurant
+
+  def initialize
+    @page_url = 'http://www.favola.si/FAVOLA,,ponudba/kosila'
+    @name = "Favola"
+  end
+
+  def menu
+    tmp = page_body.xpath("//*[@id='kosilaShow']/div")
+	.map{|div| div.text()}[Date.today.wday - 1]
+	.split(/\*\*\*/)
+	.map {|text| {text: text} }
+binding.pry
+  end
+
+end
